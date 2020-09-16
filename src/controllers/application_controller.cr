e58215e913b60892185d685d1c2febb593df91f8
Andrew <awcrotwell@gmail.com>
2020-09-16 18:20:39 -0400

    Refactoring

--- src/controllers/application_controller.cr
+++ src/controllers/application_controller.cr
@@ -3,10 +3,4 @@ require "jasper_helpers"
 class ApplicationController < Amber::Controller::Base
   include JasperHelpers
   LAYOUT = "application.slang"
-
-  def render(component : Motion::Base.class)
-    puts context.request.path
-    m(component)
-    view.to_s
-  end
 end
--- test.cr
+++ test.cr
@@ -1,14 +1,14 @@
-require "json"
+require "redis"
 
-class Test
-  include JSON::Serializable
-  @[JSON::Field(key: "count")]
-  property count : Int32 = 0
+redis = Redis.new
+redis.flushdb
 
-  def initialize(*args, **named_args)
-    pp "hit"
-  end
-end
+redis.set("motion:hggxezeqkii96lfpvl492h", "{\"motion_component\":true,\"render_hash\":14955718878113070646,\"text_content\":\"Add\",\"count\":0}\x00ButtonComponent")
 
-pp Test.new.to_json
-pp Test.from_json("{\"count\":10}")
+redis.set("motion:gkhe7qqyou5vik9e9ca2ik", "{\"motion_component\":true,\"render_hash\":8667389315068159877,\"count\":0}\x00TickerComponent")
+
+puts redis.get("motion:hggxezeqkii96lfpvl492h")
+puts redis.get("motion:gkhe7qqyou5vik9e9ca2ik")
+
+pp redis.del("motion:hggxezeqkii96lfpvl492h")
+pp redis.del("motion:gkhe7qqyou5vik9e9ca2ik")
